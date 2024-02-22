07fe9dc959fbfec5f58d904aa971b83b001fffe4
sumet <sumet.ma@ku.th>
2024-02-22 20:49:14 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:apppointment/notification_controller.dart';
+import 'package:apppointment/page/login.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() async {
+  await AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelGroupKey: "basic_channel_group",
+        channelKey: "basic_channel",
+        channelName: "Basic Notification",
+        channelDescription: "basic notification channel",
+      )
+    ],
+    channelGroups: [
+      NotificationChannelGroup(
+          channelGroupKey: "basic_channel_group",
+          channelGroupName: "Basic Group"),
+    ],
+  );
+  bool isAllowedToSendNotifications =
+      await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowedToSendNotifications) {
+    AwesomeNotifications().requestPermissionToSendNotifications();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    AwesomeNotifications().setListeners(
+        onActionReceivedMethod: NotificationController.onActionReceivedMethod,
+        onDismissActionReceivedMethod:
+            NotificationController.onDismissActionReceivesMethod,
+        onNotificationCreatedMethod:
+            NotificationController.onNotificationCreatedMethod,
+        onNotificationDisplayedMethod:
+            NotificationController.onNotificationDisplayedMethod);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.white,
+        ),
+        useMaterial3: true,
+        fontFamily: GoogleFonts.prompt().fontFamily,
+      ),
+      home: Login(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
