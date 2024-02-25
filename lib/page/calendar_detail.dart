03f474e0e1bd81ad19460d322588d320393fa8f7
sumet <sumet.ma@ku.th>
2024-02-25 11:44:28 +0700

    updated

--- lib/page/calendar_detail.dart
+++ lib/page/calendar_detail.dart
@@ -12,186 +12,207 @@ class _CalendarDetailState extends State<CalendarDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('รายละเอียด'),
+        title: const Text(
+          'รายละเอียดการนัดหมาย',
+          style: TextStyle(fontSize: 18),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  // border: Border.all(
-                  //   color: Colors.black,
-                  //   width: 2.0,
-                  // ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      spreadRadius: 3.0,
-                      blurRadius: 5.0,
-                      offset:
-                          const Offset(0.0, 1.0), // changes position of shadow
-                    ),
-                  ],
-                  color: Colors.white,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 3.0,
+                        blurRadius: 5.0,
+                        offset: const Offset(
+                            0.0, 1.0), // changes position of shadow
+                      ),
+                    ],
+                    color: Colors.white,
+                  ),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Center(
-                        child: Text(
-                          'รอวันนัดหมาย',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Color.fromARGB(255, 255, 196, 4),
-                            fontWeight: FontWeight.bold,
-                          ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "รอนัดหมาย",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromARGB(255, 255, 174, 0)),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ClipRRect(
+                                  child: Image.asset(
+                                    "assets/images/teacher1.jpg",
+                                    height: 130,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'อาจารย์อัจฉรา นามบุรี',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                              ],
+                            ),
+                            const Row(
+                              children: [
+                                Icon(Icons.date_range),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'วันที่      :  15 มกราคม 2567',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Row(
+                              children: [
+                                Icon(
+                                  Icons.access_time_sharp,
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'เวลา      :  11.00-12.30 น.',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  '',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Row(
+                              children: [
+                                Icon(Icons.business),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'สถานที่   :  ห้อง 7-317',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                Text(
+                                  '',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Divider(),
+                            const Row(
+                              children: [
+                                Text(
+                                  'เรื่อง: ',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  'ส่งรายงานความก้าวหน้าโครงงาน',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    // fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Divider(),
+                            const Row(
+                              children: [
+                                Text(
+                                  'รายละเอียด:',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            const Text(
+                              'แจ้งนิสิตในที่ปรึกษาโครงงานทุกคน ขอนัดหมายพูดคุยรายงานความก้าวหน้าโครงงานในวันที่ 15 มกราคม 2567 เวลา 11.00 น. ที่ห้องพักอาจารย์ค่ะ',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            Divider(),
+                          ],
                         ),
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Image.asset(
-                            "assets/images/teacher.png",
-                            height: 100,
-                            width: 97,
-                          ),
-                          const Text(
-                            '     อาจารย์ อัจฉรา นามบุรี',
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Row(
-                        children: [
-                          // Icon(Icons.calendar_month),
-                          // SizedBox(
-                          //   width: 10,
-                          // ),
-                          Text(
-                            '   วันที่        :   15 มกราคม 2567',
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          // Text(
-                          //   '   15 มกราคม 2567',
-                          //   style: TextStyle(
-                          //     fontSize: 18,
-                          //   ),
-                          // ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Row(
-                        children: [
-                          // Icon(Icons.watch_later),
-                          // SizedBox(
-                          //   width: 10,
-                          // ),
-
-                          Text(
-                            '   เวลา        :   11.00-12.30 น.',
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          Text(
-                            '',
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Row(
-                        children: [
-                          // Icon(Icons.business),
-                          // SizedBox(
-                          //   width: 10,
-                          // ),
-                          Text(
-                            '   สถานที่     :   อาคาร 7 ห้อง 114/2',
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          Text(
-                            '',
-                            style: TextStyle(
-                              fontSize: 18,
-                              // fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Row(
-                        children: [
-                          Text(
-                            'เรื่อง ',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            'ปรึกษาโครงงานจบการศึกษา',
-                            style: TextStyle(
-                              fontSize: 18,
-                              // fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Text(
-                        'รายละเอียด',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Text(
-                        'นัดหมายเพื่อสอบถามเกี่ยวกับโครงงานจบการศึกษาค่ะ',
-                        style: TextStyle(
-                          fontSize: 18,
-                          // fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
